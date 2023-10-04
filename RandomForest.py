731cdb569a2effa4312eee91f46de3e318631c05
Renseck <rensvaneck@gmail.com>
2023-10-04 16:04:19 +0200

    Large changes to function structure and additional functions.
    Added function to make running new instances of interest easier - numbers of trees, different/additional features.

--- RandomForest.py
+++ RandomForest.py
@@ -7,12 +7,21 @@ Created on Mon Sep 25 11:37:09 2023
 import sklearn
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split, GridSearchCV
-from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
-from scipy import stats
+from sklearn.metrics import mean_squared_error, r2_score
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from itertools import chain
+from tabulate import tabulate
+
+full_variable_names = {"wd": "Wind direction",
+              "ws": "Wind speed", 
+              "t": "Temperature",
+              "q": "Global radiation",
+              "hourly_rain": "Precipitation (hourly)",
+              "p": "Pressure",
+              "n": "Cloud cover",
+              "rh": "Relative humidity"}
 
 class Site():
     # This is just used to store some site-specific information, in case it ever
@@ -32,35 +41,6 @@ class Site():
         self.siteMunicipality = siteMunicipality
         self.siteProvince = siteProvince
         self.siteArea = siteArea
-
-def hypertune(grid, x_data, y_data, scoring = None):
-    """
-    Tune hyperparameters of sklearn randomforestregressor.
-
-    Parameters
-    ----------
-    grid : DICT
-        Parameters (with ranges) to be optimized.
-    x_data : DATAFRAME
-        X-data to be trained.
-    y_data : DATAFRAME
-        Y-data to be trained.
-    scoring : STR, optional
-        Scoring scheme to use. The default is None.
-
-    Returns
-    -------
-    sklearn randomforestregressor
-        Tuned random forest regressor model.
-
-    """
-    rf = RandomForestRegressor()
-    grid_search = GridSearchCV(estimator = rf, param_grid = grid,
-                               cv = 5, n_jobs = -1, scoring = scoring, verbose = 0)
-    grid_search.fit(x_data, y_data)
-    
-    print("Best parameters: {}".format(grid_search.best_params_))
-    return grid_search.best_estimator_
 
 def clean_data(dataframe, verbose = True):
     """
@@ -122,12 +102,330 @@ def clean_data(dataframe, verbose = True):
 
     return dataframe
 
+def hypertune(grid, x_data, y_data, scoring = None):
+    """
+    Tune hyperparameters of sklearn randomforestregressor.
+
+    Parameters
+    ----------
+    grid : DICT
+        Parameters (with ranges) to be optimized.
+    x_data : DATAFRAME
+        X-data to be trained.
+    y_data : DATAFRAME
+        Y-data to be trained.
+    scoring : STR, optional
+        Scoring scheme to use. The default is None.
+
+    Returns
+    -------
+    sklearn randomforestregressor
+        Tuned random forest regressor model.
+
+    """
+    rf = RandomForestRegressor()
+    grid_search = GridSearchCV(estimator = rf, param_grid = grid,
+                               cv = 5, n_jobs = -1, scoring = scoring, verbose = 0)
+    grid_search.fit(x_data, y_data)
+    
+    print("Best parameters: {}".format(grid_search.best_params_))
+    return grid_search.best_estimator_
+
+# Credit to https://www.kaggle.com/code/dedecu/cross-correlation-time-lag-with-pandas/notebook
+def df_derived_by_shift(df,lag=0,NON_DER=[]):
+    """
+    Generate time-shifted columns.
+
+    Parameters
+    ----------
+    df : DATAFRAME
+        Input DataFrame with columns to-be-shifted.
+    lag : INT, optional
+        Number of time shifts (up to and including argument). The default is 0.
+    NON_DER : LIST, optional
+        Columns to be excluded from shifting. The default is [].
+
+    Returns
+    -------
+    df : DATAFRAME
+        DataFrame with included time shifted columns.
+
+    """
+    df = df.copy()
+    if not lag:
+        return df
+    cols ={}
+    for i in range(1,lag+1):
+        for x in list(df.columns):
+            if x not in NON_DER:
+                if not x in cols:
+                    cols[x] = ['{}_{}'.format(x, i)]
+                else:
+                    cols[x].append('{}_{}'.format(x, i))
+    for k,v in cols.items():
+        columns = v
+        dfn = pd.DataFrame(data=None, columns=columns, index=df.index)    
+        i = 1
+        for c in columns:
+            dfn[c] = df[k].shift(periods=i)
+            i+=1
+        df = pd.concat([df, dfn], axis=1).reindex(df.index)
+    return df
+
+def show_cross_correlation(df, target):
+    """
+    Shows maximum correlation for time shifted variables and target.
+
+    Parameters
+    ----------
+    df : DATAFRAME
+        Input dataframe containing variables and target.
+    target : STRING
+        Target variable (column name).
+
+    Returns
+    -------
+    correls : LIST
+        List of maximally correlated variable per "category".
+
+    """
+    df_new = df_derived_by_shift(df, 6, ["date", "date_end", target])
+    correlation = df_new.corr()
+    meteo_vars = ["wd", "ws", "t", "q", "hourly_rain", "p", "n", "rh"]
+    
+    correls = []
+    print("Value after underscore shows the amount of time-steps. I.e., 1 means shifted by 1 step/hour.")
+    for meteo_var in meteo_vars:
+        correl = correlation.loc[target].filter(regex = meteo_var)
+        max_correl = correl.abs().idxmax()
+        #print(f"{correl}")
+        print(f"Max correlation between {meteo_var} and {target}: {max_correl}")
+        correls.append(max_correl)
+        
+    return correls
+
+def add_shifts(df, train_cols, target):
+    """
+    Add time shifted variables to DataFrame.
+
+    Parameters
+    ----------
+    df : DATAFRAME
+        Input DataFrame containing variables and target.
+    train_cols : LIST
+        List of training columns.
+    target : STRING
+        Name of target.
+
+    Returns
+    -------
+    df : DATAFRAME
+        DataFrame with shifted columns added in.
+    train_cols : LIST
+        List of "new" training columns.
+
+    """
+    max_correl_shifts = show_cross_correlation(df, target)
+    
+    for new_col in max_correl_shifts:
+        splitup = new_col.split("_")
+        if len(splitup) == 1:
+            pass
+        elif len(splitup) == 2:
+            df[new_col] = df[splitup[0]].shift(int(splitup[-1]))
+            train_cols.append(new_col)
+        else:
+            df[new_col] = df[splitup[0] + "_" + splitup[1]].shift(int(splitup[-1]))
+            train_cols.append(new_col)    
+            
+    df = df.dropna()    
+
+    return df, train_cols
+
+def run(df, num_trees, features, plot = True):
+    """
+    Single model run
+
+    Parameters
+    ----------
+    df : DATAFRAME
+        DataFrame containing training and target data.
+    num_trees : INT
+        Number of trees for RandomForestRegressor model.
+    features : LIST
+        What features to train on.
+    plot : BOOL, optional
+        Show plots or not. The default is True.
+
+    Returns
+    -------
+    y_pred_train : NP.ARRAY
+        Array of predicted data in training period.
+    y_pred : NP.ARRAY
+        Array of predicted data in prediction period.
+
+    """
+    start_index = 0
+    end_index = df["date_end"][df["date_end"] == pd.to_datetime("2018-01-01 00:00:00")].index[0]
+    df_train = df[start_index:end_index]
+    df_pred = df[end_index:]
+    
+    X = df_train[features]
+    y = df_train[target]
+
+    x_train, x_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 42)
+    x_pred = df_pred[features]
+    y_hat = df_pred[target].values # For comparison
+
+    # Define the Random Forest model
+    model = RandomForestRegressor(n_estimators=num_trees, min_samples_leaf=5, random_state=42)
+
+    # Fit the Random Forest model on the whole dataset
+    model.fit(x_train, y_train) 
+
+    # Determine the R2 score of the models
+    train_score = model.score(x_train, y_train)
+    test_score = model.score(x_test, y_test)
+    # print('R2 train score: {:6.3f}' .format(train_score))
+    # print('R2 test score: {:6.3f}' .format(test_score))
+    # (Severe) overfitting?
+
+    # Make a prediction for x_pred for 2018
+    y_pred_train = model.predict(X)
+    y_pred = model.predict(x_pred)
+
+    # Plot data and calculate statistics by comparing y_pred with observation data
+    #==========================================
+    if plot:
+        # Plot training period:
+        plt.figure(figsize = (10,7))
+        plt.title(r"Ozone concentration ($n_{{estimators}} = {{{n_estimators}}}$) (Training)".format(n_estimators = model.n_estimators))
+    
+        plt.plot(df_train["date"], y_pred_train, label = "Model")
+        plt.plot(df_train["date"], y, label = "Observation", alpha = 0.5)
+        plt.xlabel("Date")
+        plt.xticks(rotation = 45)
+        plt.ylabel("Concentration $[\mu$g $m^{-3}]$")
+        plt.legend()
+        plt.show()
+        print(f"[TRAINING (trees = {model.n_estimators})] R2: {r2_score(y, y_pred_train):6.3f}")
+        print(f"[TRAINING (trees = {model.n_estimators})] RMSE: {mean_squared_error(y, y_pred_train, squared = False):6.3f}")
+        print(f"[TRAINING (trees = {model.n_estimators})] Correlation: {np.corrcoef(y, y_pred_train)[0][1]:6.3f}")
+    
+        # Plot prediction period (2018)
+        plt.figure(figsize = (10,7))
+        plt.title(r"Ozone concentration ($n_{{estimators}} = {{{n_estimators}}}$) (Prediction)".format(n_estimators = model.n_estimators))
+    
+        plt.plot(df_pred["date"], y_pred, label = "Model")
+        plt.plot(df_pred["date"], y_hat, label = "Observation", alpha = 0.5)
+    
+        plt.xlabel("Date")
+        plt.xticks(rotation = 45)
+        plt.ylabel("Concentration $[\mu$g $m^{-3}]$")
+        plt.legend()
+        plt.show()
+        print(f"[PREDICTION (trees = {model.n_estimators})] R2: {r2_score(y_hat, y_pred):6.3f}")
+        print(f"[PREDICTION (trees = {model.n_estimators})] RMSE: {mean_squared_error(y_hat, y_pred, squared = False):6.3f}")
+        print(f"[PREDICTION (trees = {model.n_estimators})] Correlation: {np.corrcoef(y_hat, y_pred)[0][1]:6.3f}")
+
+    return y_pred_train, y_pred
+
+def run_singles(df, num_trees, features):
+    """
+    Many runs of single features in [features].
+
+    Parameters
+    ----------
+    df : DATAFRAME
+        DataFrame containing training and target data.
+    num_trees : INT
+        Number of trees for RandomForestRegressor model.
+    features : LIST
+        What features to train on.
+    plot : BOOL, optional
+        Show plots or not. The default is True.
+
+    Returns
+    -------
+    results_dict : DICT
+        Predicted data trained on only given key.
+
+    """
+    start_index = 0
+    end_index = df["date_end"][df["date_end"] == pd.to_datetime("2018-01-01 00:00:00")].index[0]
+    df_train = df[start_index:end_index]
+    df_pred = df[end_index:]
+
+    X = df_train[features]
+    y = df_train[target]
+
+    x_train, x_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 42)
+    x_pred = df_pred[features]
+    y_hat = df_pred[target].values # For comparison
+
+    results_dict = {"y_pred_train": {}, "y_pred": {}}
+    for feature in features:
+        feature_name = feature
+        y_pred_train, y_pred = run(df, num_trees, [feature], plot = False)
+        results_dict["y_pred_train"][feature] = y_pred_train
+        results_dict["y_pred"][feature] = y_pred
+
+    fig, axd = plt.subplot_mosaic([["wd", "ws", "t", "q"],
+                                  ["hourly_rain", "p", "n", "rh"]], figsize = (11,8))
+
+    table = []
+    for key in axd.keys():
+        axd[key].set_title(full_variable_names[key])
+        axd[key].plot(df_train["date"], results_dict["y_pred_train"][key], label = "Model")
+        axd[key].plot(df_train["date"], y, label = "Observation", alpha = 0.5)
+        axd[key].tick_params("x", labelrotation = 45)
+        
+        rsquared = r2_score(y, results_dict["y_pred_train"][key])
+        rmse = mean_squared_error(y, results_dict["y_pred_train"][key], squared = False)
+        corr = np.corrcoef(y, results_dict["y_pred_train"][key])[0][1]
+        table.append([full_variable_names[key], rsquared, rmse, corr])
+        
+    plt.tight_layout(rect = [0, 0.03, 1, 0.95])
+    axd["q"].legend(bbox_to_anchor = (1.05, 1.0))
+    fig.suptitle("Effect of single variables on modelled data (training set)")
+    plt.show()
+    
+    # Sort table from highest to lowest R2 score.
+    table.sort(key = lambda x: x[1], reverse = True)
+    print(tabulate(table, headers = ["Variable", "R^2", "RMSE", "Correlation"]))
+    
+    return results_dict
+
+def bonus_run(df, train_cols, target):
+    """
+    This run includes time-shifted variables, based on which time-shift has the highest correlation
+    to the observed target variable. The model is then trained on these additional shifted variables.
+
+    Parameters
+    ----------
+    df : DATAFRAME
+        Input dataframe containing (to-be-shifted) variables and target.
+    train_cols : LIST
+        List of training column names.
+    target : STRING
+        Name of target variable.
+
+    Returns
+    -------
+    None.
+
+    """
+    df, train_pred_cols = add_shifts(df, train_cols, target)
+    y_pred_train, y_train = run(df, 30, train_pred_cols, True)
+    
+    
 # %%% The start of the actual stuff
 
 # Read the data into a dataframe
-df = pd.read_csv("Rural_NL10644-AQ-METEO.csv", sep=";")
+df = pd.read_csv("data\\Rural_NL10644-AQ-METEO.csv", sep=";")
 target = "o3"
-relevant_cols = ["date", "date_end", "wd", "ws", "t", "q", "hourly_rain", "p", "n", "rh", target]
+train_pred_cols = ["wd", "ws", "t", "q", "hourly_rain", "p", "n", "rh"]
+relevant_cols = ["date", "date_end"] + train_pred_cols + [target]
 
 # Split off unnecessarry columns but store them in the Site class
 siteKeys = ["site", "name", "lat", "lon", "organisation", "type", "site_eu", "type_alt",
@@ -143,93 +441,22 @@ df["date"] = pd.to_datetime(df["date"])
 df["date_end"] = pd.to_datetime(df["date_end"])
 
 df = clean_data(df[relevant_cols])
-    
-# Select the training and prediction subsets; define parameter tuning grid.
+
 start_index = 0
 end_index = df["date_end"][df["date_end"] == pd.to_datetime("2018-01-01 00:00:00")].index[0]
 df_train = df[start_index:end_index]
 df_pred = df[end_index:]
 
-param_grid = {
-    'bootstrap': [True, False],
-    'max_depth': [15, 20, 25],
-    'max_features': ["auto", "sqrt"],
-    'min_samples_leaf': [3, 5, 8],
-    'min_samples_split': [2, 5, 8],
-    'n_estimators': [30, 40, 50]
-}
-
-# Read in the data for training (x_train, y_train for 2015-2017) and for prediction (x_pred for 2018)
-#x_train(n_data, n_variables)  = 2d-array with explanatory variables 
-#y_train(n_data)                      = 1d-array with observations
-#x_pred(n_data2, n_variables)= 2d-array with explanatory variables 
-
-X = df_train[["wd", "ws", "t", "q", "hourly_rain", "p", "n", "rh"]]
+X = df_train[train_pred_cols]
 y = df_train[target]
 
 x_train, x_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 42)
-x_pred = df_pred[["wd", "ws", "t", "q", "hourly_rain", "p", "n", "rh"]]
-y_hat = df_pred[target].values # For comparison
+x_pred = df_pred[train_pred_cols]
+y_hat = df_pred[target].values # For comparison    
 
-# Define the Random Forest model
-model = RandomForestRegressor(n_estimators=30, min_samples_leaf=5, random_state=42)
-tuned_model = hypertune(param_grid, x_train, y_train, scoring = "r2")
+run(df, 30, features = train_pred_cols, plot = True)
+run(df, 20, features = train_pred_cols, plot = True)
+run(df, 40, features = train_pred_cols, plot = True)
+run_singles(df, 30, train_pred_cols)
 
-# Fit the Random Forest model on the whole dataset
-model.fit(x_train, y_train) 
-
-# Determine the R2 score of the models
-train_score = model.score(x_train, y_train)
-test_score = model.score(x_test, y_test)
-tuned_train_score = tuned_model.score(x_train, y_train)
-tuned_test_score = tuned_model.score(x_test, y_test)
-print('R2 train score: {:6.3f}' .format(train_score))
-print('R2 test score: {:6.3f}' .format(test_score))
-print("Tuned R2 train score: {:6.3f}".format(tuned_train_score))
-print("Tuned R2 test score: {:6.3f}".format(tuned_test_score))
-# Severe overfitting?
-
-# Make a prediction for x_pred for 2018
-y_pred = model.predict(x_pred)
-y_pred_tuned = tuned_model.predict(x_pred)
-
-# Plot data and Calculate statistics by comparing y_pred with observation for 2018
-#==========================================
-plt.figure(figsize = (10,7))
-plt.title(r"Ozone concentration ($n_{{estimators}} = {{{n_estimators}}}$)".format(n_estimators = model.n_estimators))
-
-plt.plot(df_pred["date"], y_pred, label = "Prediction")
-plt.plot(df_pred["date"], y_pred_tuned, label = "Prediction tuned", alpha = 0.5)
-plt.plot(df_pred["date"], y_hat, label = "Observation", alpha = 0.5)
-
-plt.xlabel("Date")
-plt.xticks(rotation = 45)
-plt.ylabel("Concentration $[\mu$g $m^{-3}]$")
-plt.legend()
-plt.show()
-
-rsquared = r2_score(y_hat, y_pred)
-rsquared_tuned = r2_score(y_hat, y_pred_tuned)
-print(f"Default R2: {rsquared:6.3f}")
-print(f"Tuned R2: {rsquared_tuned:6.3f}")
-
-# Can we do some accuracy testing here? Maybe using accuracy_score, confusion matrix...
-# Look into the overfitting as well - it may be an indicator of something going on.
-
-# Collect performance metric scores for default and tuned models
-default_scores = [rsquared, mean_squared_error(y_hat, y_pred, squared = False),
-                  mean_absolute_error(y_hat, y_pred)]  # Replace with your actual scores
-tuned_scores = [rsquared_tuned, mean_squared_error(y_hat, y_pred_tuned, squared = False),
-                mean_absolute_error(y_hat, y_pred_tuned)]    # Replace with your actual scores
-
-# Perform a paired t-test
-t_stat, p_value = stats.ttest_rel(tuned_scores, default_scores)
-
-# Set your significance level (alpha)
-alpha = 0.05
-
-# Check if the p-value is less than alpha
-if p_value < alpha:
-    print("Reject the null hypothesis: There is a statistically significant difference.")
-else:
-    print("Fail to reject the null hypothesis: There is no statistically significant difference.")
+bonus_run(df, train_pred_cols, target)
